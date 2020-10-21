5ecb69ff0c3ccc4771cfd9e128130e54063f5946
Wowa <wowabbs@gmail.com>
2020-10-22 01:47:55 +0300

    Tools: Update files

--- Builder/Lib/Using.php
+++ Builder/Lib/Using.php
@@ -1,8 +1,8 @@
 <?
-  if(file_exists(DirName(__FILE__).'/WLib/Using.php'))
+  if(file_exists(__DIR__.'/WLib/Using.php'))
     Include_Once('WLib/Using.php');
   else
     return Include_Once '_Using.php';
   
-  $Loader->AddSearchPath(DirName(__FILE__)); 
+  $Loader->AddSearchPath(__DIR__);
 ?>
